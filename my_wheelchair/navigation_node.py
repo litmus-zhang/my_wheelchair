7d0e40813916097056c2cfc613246000183af81f
litmus-zhang <abdulsalamlukmon9@gmail.com>
2024-10-15 13:12:37 +0000

    audio setup completed

--- my_wheelchair/navigation_node.py
+++ my_wheelchair/navigation_node.py
@@ -3,6 +3,7 @@ from rclpy.node import Node
 from geometry_msgs.msg import Twist, PoseStamped
 from std_msgs.msg import String
 import RPi.GPIO as GPIO
+import time
 
 class NavigationNode(Node):
     def __init__(self):
@@ -25,6 +26,7 @@ class NavigationNode(Node):
         self.locations = self.load_locations()
         # GPIO setup
         GPIO.setmode(GPIO.BCM)
+        GPIO.setwarnings(False)
         self.front_wheel_pwm_pin = 17
         self.front_wheel_left_dir_pin = 27
         self.front_wheel_right_dir_pin = 23
@@ -67,6 +69,25 @@ class NavigationNode(Node):
                 self.send_goal(coordinates)
                 return
         self.get_logger().info(f"Unknown location in command: {command}")
+    
+    def move_forward_backward(self):
+        GPIO.output(self.front_wheel_left_dir_pin, GPIO.HIGH )
+        GPIO.output(self.rear_wheel_forward_dir_pin, GPIO.HIGH )
+        print("Turning left")
+        time.sleep(5)
+        GPIO.output(self.front_wheel_left_dir_pin, GPIO.LOW )
+        GPIO.output(self.rear_wheel_forward_dir_pin, GPIO.LOW )
+        
+        time.sleep(5)
+        GPIO.output(self.front_wheel_right_dir_pin, GPIO.HIGH )
+        GPIO.output(self.rear_wheel_backward_dir_pin, GPIO.HIGH )
+        print("Turning RIght")
+        time.sleep(5)
+        GPIO.output(self.front_wheel_right_dir_pin, GPIO.LOW )
+        GPIO.output(self.rear_wheel_backward_dir_pin, GPIO.LOW )
+        
+        
+        
         
 
     def send_goal(self, coordinates):
@@ -82,20 +103,22 @@ class NavigationNode(Node):
         
         
     def set_motor_speed(self, left_speed, right_speed):
-        # Set left motor
+        # Set front motor
         GPIO.output(self.front_wheel_left_dir_pin, GPIO.HIGH if left_speed >= 0 else GPIO.LOW)
         GPIO.output(self.front_wheel_right_dir_pin, GPIO.HIGH if left_speed >= 0 else GPIO.LOW)
         self.front_wheel_pwm_pin.ChangeDutyCycle(abs(left_speed) * 100)
         
-        # Set right motor
+        # Set rears motors
         GPIO.output(self.rear_wheel_forward_dir_pin, GPIO.HIGH if right_speed >= 0 else GPIO.LOW)
         GPIO.output(self.rear_wheel_backward_dir_pin, GPIO.HIGH if right_speed >= 0 else GPIO.LOW)
         self.rear_wheel_pwm_pin.ChangeDutyCycle(abs(right_speed) * 100)
 
 def main(args=None):
     print("Hello from my_wheelchair, navigation node")
+    
     rclpy.init(args=args)
     node = NavigationNode()
+    node.move_forward_backward()
     rclpy.spin(node)
     node.destroy_node()
     rclpy.shutdown()
--- my_wheelchair/voice_recognition_node.py
+++ my_wheelchair/voice_recognition_node.py
@@ -2,14 +2,20 @@ import rclpy
 from rclpy.node import Node
 from std_msgs.msg import String
 import speech_recognition as sr
+import pyaudio
+
 
 class VoiceRecognitionNode(Node):
     def __init__(self):
         super().__init__('voice_recognition_node')
         self.publisher_ = self.create_publisher(String, 'voice_command', 10)
+        pa = pyaudio.PyAudio()
+        default_input_device_info = pa.get_default_input_device_info()
+        device_index = default_input_device_info['index']
+        print(f"device_index: {device_index}")
         self.timer = self.create_timer(1.0, self.listen_for_command)
         self.recognizer = sr.Recognizer()
-        self.microphone = sr.Microphone()
+        self.microphone = sr.Microphone(device_index=16)
 
     def listen_for_command(self):
         with self.microphone as source:
@@ -17,18 +23,23 @@ class VoiceRecognitionNode(Node):
             self.get_logger().info('Listening for a command...')
             audio = self.recognizer.listen(source)
 
-        try:
-            command = self.recognizer.recognize_google(audio)
-            self.get_logger().info(f'Recognized command: {command}')
-            msg = String()
-            msg.data = command
-            self.publisher_.publish(msg)
-        except sr.UnknownValueError:
-            self.get_logger().info('Could not understand the audio')
-        except sr.RequestError as e:
-            self.get_logger().error(f'Could not request results; {e}')
+            try:
+                command = self.recognizer.recognize_sphinx(audio)
+                self.get_logger().info(f'Recognized command: {command}')
+                msg = String()
+                msg.data = command
+                self.publisher_.publish(msg)
+            except sr.UnknownValueError:
+                self.get_logger().info('Could not understand the audio')
+            except sr.RequestError as e:
+                self.get_logger().error(f'Could not request results; {e}')
 
 def main(args=None):
+    pa = pyaudio.PyAudio()
+    print("Hello from my_wheelchair, voice recognition node")
+    default_input_device_info = pa.get_default_input_device_info()
+    device_index = default_input_device_info['index']
+    print(f"device index >> {device_index}")
     rclpy.init(args=args)
     node = VoiceRecognitionNode()
     rclpy.spin(node)
